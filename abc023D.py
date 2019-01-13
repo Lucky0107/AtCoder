451e5d9ecc808380fdf0d352eb3ae1ab542782e4
Lucky0107 <tmiya0107@gmail.com>
2019-01-13 18:29:52 +0900

    no message

--- abc023D.py
+++ abc023D.py
@@ -1,42 +1,29 @@
+import numpy as np
+
 N = int(input())
-H = [0]*N
-S = [0]*N
- 
+H = np.zeros(N, dtype=np.int)
+S = np.zeros(N, dtype=np.int)
+th = np.arange(N)
+
 for i in range(N):
     H[i], S[i] = map(int, input().split())
  
 def check_P(p):
     #スコアP内で撃ち落とせるかをチェックする
     #pに達するまでの時間を求めて早い順に撃っていく。
-    T = [0]*N
-    for i in range(N):
-        T[i] = int((p - H[i]) / S[i])
-    sorted_index_T = sorted(range(len(T)), key=lambda k: T[k])
-    for (k,i) in enumerate(sorted_index_T):
-        if (T[i] < k):
-            return False
-    return True
+    T = np.floor((p - H) / S)
+    return np.all(np.sort(T) >= th)
  
 #pは2分探索で探す
-P_low = 0
-P_high = 0
-for i in range(N):
-    p = H[i]
-    if (P_low < p):
-        P_low = p
-    p = H[i] + S[i]*(N-1)
-    if (P_high < p):
-        P_high = p
-P_low -= 1
- 
-while(True):
-    P_now = int((P_low + P_high) / 2)
-    #print(_, P_low, P_now, P_high)
+P_low = np.max(H) - 1
+P_high = np.max(H+S*(N-1))
+
+while((P_high - P_low) > 1):
+    P_now = np.int((P_low + P_high) / 2)
+    #print(P_low, P_now, P_high)
  
     if (check_P(P_now)):
         P_high = P_now
     else:
         P_low = P_now
-    if (P_high <= (P_low + 1)):
-        print(P_high)
-        break
+print(P_high)
